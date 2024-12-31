02c91fed0c06290c4260ae04ff6482681ea2afc7
rushil <rushilverma7@gmail.com>
2025-01-01 00:11:41 +0530

    all done

--- home/url.py
+++ home/url.py
@@ -3,7 +3,7 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path("index", views.index, name= 'home'),
+    path("index", views.index, name= 'index'),
     path("about", views.about, name= 'about'),
     path("services", views.services, name= 'services'),
     path("contact", views.contact, name= 'contact'),
